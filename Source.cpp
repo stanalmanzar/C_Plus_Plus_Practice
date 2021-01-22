36092373e00322ec8ed624afe02be5c3b5b2dee4
stanalmanzar <58785064+stanalmanzar@users.noreply.github.com>
2021-01-21 23:33:53 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	// The idea is to covert a pennie total to a value of quareter , nicklels , dimes , and pennies 
+	int pennie
+		cout << "Pennies to coin: This programs design is to convert pennie to coin: plese enters a whole digit to convert to coin";
+
+		//input cents to a value
+		cin >> pennie;
+
+
+
+	system("pause");
+}
