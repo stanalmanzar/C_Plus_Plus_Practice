57d1d573d3ba42c8658bc8e8c837de7a748239e6
stanalmanzar <58785064+stanalmanzar@users.noreply.github.com>
2021-01-21 23:43:54 -0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,9 +5,10 @@ int main()
 	// The idea is to covert a pennie total to a value of quareter , nicklels , dimes , and pennies 
 	int pennie
 		cout << "Pennies to coin: This programs design is to convert pennie to coin: plese enters a whole digit to convert to coin";
-
 		//input cents to a value
 		cin >> pennie;
+		if pennie < 1;
+		count << "you have entered a negative number" << endl;
 
 
 
